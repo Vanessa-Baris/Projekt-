a6be02e17b6fd3cfc53c591668d702e7b3bc37a5
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-09-20 16:03:45 +0200

    1.1

--- scr/Spiel.kt
+++ scr/Spiel.kt
@@ -1,5 +1,7 @@
 class Spiel() {
 
+
+
     var list = mutableListOf<Spielfeld>(
         Spielfeld(false , false) ,
         Spielfeld(false , true) ,
@@ -14,6 +16,22 @@ class Spiel() {
                 spielfeld.richtungsauswahl()
                 spielfeld.kreuzung1()
             }
+        }
+    }
+
+
+    fun kreuzung1(list: mu){
+
+        var richtung = richtungsauswahl()
+
+        if (richtung == "Osten") {
+            println("Du gehst nach Osten und kommst zu einer neuen Kreuzung ${spielfeld3()}")
+        } else if (richtung == "Westen") {
+            println("Du gehst nach Westen und wirst zurück zum Anfang katapultiert. ${spielfeld1()}")
+        } else if (richtung == "Süden") {
+            println("Du gehst nach Süden und kommst an eine $wand1")
+        } else {
+            println("Ungültige Eingabe. Bitte wähle 'Osten', 'Westen' oder 'Süden'.")
         }
     }
 
