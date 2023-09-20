fdf3a6120338d4bc810f1aae8cdf3a95d94251f6
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-09-20 10:28:07 +0200

    1.1

--- scr/Kreuzung.kt
+++ scr/Kreuzung.kt
@@ -1,2 +1,20 @@
-open class Kreuzung() {
+open class Kreuzung(): Spielfeld() {
+
+    fun kreuzung1(){
+        var richtung = ""
+        while (richtung != "Osten" && richtung != "Westen" && richtung != "Süden") {
+            print("Deine Wahl: ")
+            richtung = readln()
+
+            if (richtung == "Osten") {
+                println("Du gehst nach Osten und kommst zu einer neuen Kreuzung ${spielfeld3()}")
+            } else if (richtung == "Westen") {
+                println("Du gehst nach Westen und wirst zurück zum Anfang katapultiert. ${spielfeld1()}")
+            } else if (richtung == "Süden") {
+                println("Du gehst nach Süden und ")
+            } else {
+                println("Ungültige Eingabe. Bitte wähle 'Osten', 'Westen' oder 'Süden'.")
+            }
+        }
+    }
 }
