688897c966a525bff674a71ecb01f4955a5bdebb
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-09-20 14:56:40 +0200

    1.1

---
+++ scr/Erklärung.kt
@@ -0,0 +1,17 @@
+/*
+Es gibt Wände, Fallen, Spielfelder, ein Spiel
+
+Ich möchte in die Main nur Funktionsaufrufe machen , keine fun etc
+
+Ich möchte die Fun Kreuzung 1 , Kreuzung 2 etc in den einzelnen Spielfeldern aufrufen
+
+in den Kreuzungen werden je nachdem welcher Weg man wählt nochmal andere Funktionen die ich in den Klassen definiere aufgerufen
+
+Man hat 5 Hp Punkte
+
+Bei Fallen werden 1 Punkt abgezogen
+
+Einfach und schlank wie möglich, darauf liegt mein Fokus
+
+
+ */
