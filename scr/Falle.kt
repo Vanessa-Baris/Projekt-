1e3eb8cd0cf3fc3989a6cc4e92ec6da6660ab609
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-09-22 14:51:21 +0200

    1.1

--- scr/Falle.kt
+++ scr/Falle.kt
@@ -9,7 +9,6 @@ open class Falle(text:String , var damage: Int , var riddle: Boolean): Spielfeld
     }
 
     fun riddle(){
-        //Antwort Sam
         println("Dein Rätsel: Sams Vater hat 3 Töchter. Zwei von ihnen heißen Stephanie und Amanda. Wie lautet der Name der dritten Tochter?")
         var answer: String = readln()
         if (answer == "Sam") {
