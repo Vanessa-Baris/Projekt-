80dd64bfe4a0bf5275db18e62efe908fb703fb21
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-09-22 11:58:30 +0200

    1.1

--- scr/Spiel.kt
+++ scr/Spiel.kt
@@ -12,7 +12,9 @@ open class Spiel() {
             Spieler("" , 5).spielerAuswahl()
             Thread.sleep(3000)
             aktuellesSpielfeld = aktuellesSpielfeld.nächstesSpielfeld()
+            aktuellesSpielfeld = aktuellesSpielfeld.richtungsauswahl()
             aktuellesSpielfeld = wand1.nächstesSpielfeld()
+            aktuellesSpielfeld = aktuellesSpielfeld.richtungsauswahl()
             //funktioniert das so?:
             wand2.wand2(5)
             aktuellesSpielfeld = wand2.nächstesSpielfeld()
