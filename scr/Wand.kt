8f2984ceec8a0c17786caf9ab3d53aa4dd250413
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-09-22 11:55:27 +0200

    1.1

--- scr/Wand.kt
+++ scr/Wand.kt
@@ -2,8 +2,9 @@ open class Wand(text: String): Spielfeld(text) {
 
 
    open fun wand2(hp: Int){
-        var aktuelleHp = hp - 1
-        var hp = aktuelleHp
+        val aktuelleHp = hp - 1
+        val hp: Int = aktuelleHp
+        println("Aktuelle Lebenspunkte: $hp")
 
     }
 }
