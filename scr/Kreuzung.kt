024b696e215431e66a150c50bd42b70b788a5352
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-09-22 11:56:35 +0200

    1.1

--- scr/Kreuzung.kt
+++ scr/Kreuzung.kt
@@ -1,5 +1,6 @@
 class Kreuzung(text: String): Spielfeld(text) {
 
+    //muss ich mir noch überlegen wie man das geschickter macht:
     override fun nächstesSpielfeld(): Spielfeld {
         this.richtungsauswahl()
         return super.nächstesSpielfeld()
