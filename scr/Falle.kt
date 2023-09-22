af5f464b6b5146b7121dbe6ffc7298eefb703bb3
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-09-22 14:36:09 +0200

    1.1

--- scr/Falle.kt
+++ scr/Falle.kt
@@ -1,6 +1,12 @@
-open class Falle(text:String , damage: Int , riddle: Boolean): Spielfeld(text) {
-
-   // fun damage
+open class Falle(text:String , var damage: Int , var riddle: Boolean): Spielfeld(text) {
+    fun damage2(spieler: Spieler) {
+        if (spieler.hp > 0) {
+            spieler.hp -= damage
+            println("Aktuelle Lebenspunkte: ${spieler.hp}")
+        } else {
+            println("Du hast kein verfügbares Leben mehr. Du hast verloren.")
+        }
+    }
 
 
 }
