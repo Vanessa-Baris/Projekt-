6879f893e9eade99c30f4c306b708a5963ed0b3c
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-09-25 10:57:56 +0200

    1.1

--- scr/Kreuzung.kt
+++ scr/Kreuzung.kt
@@ -1,5 +1,8 @@
 class Kreuzung(text: String): Spielfeld(text) {
 
+    override var vorherigesSpielfeld: Spielfeld? = null
+
+
     //muss ich mir noch überlegen wie man das geschickter macht:
     override fun nächstesSpielfeld(): Spielfeld {
         this.richtungsauswahl()
