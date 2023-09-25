6b51e43a686052841d5d3b5820291491fe497e02
Vanessa-Baris <137050918+Vanessa-Baris@users.noreply.github.com>
2023-09-25 10:54:26 +0200

    1.1

--- scr/Wand.kt
+++ scr/Wand.kt
@@ -8,18 +8,4 @@ open class Wand(text: String , var damage: Int): Spielfeld(text) {
             println("Du hast kein verfügbares Leben mehr. Du hast verloren.")
         }
     }
-
-    //Überflüssig?
-    fun damage2(spieler: Spieler) {
-        if (spieler.hp > 0) {
-            spieler.hp -= damage
-            println("Aktuelle Lebenspunkte: ${spieler.hp}")
-        } else {
-            println("Du hast kein verfügbares Leben mehr. Du hast verloren.")
-        }
-    }
-
-
-
-
 }
